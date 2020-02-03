5dfccd49c30eeabbe9d26347ba4735d4ba5bf021
Luis Rosado <luismrosado@yahoo.com>
2020-02-03 08:38:38 +0000

    From variables to math

--- hello.rb
+++ hello.rb
@@ -8,3 +8,12 @@
 # lname = gets
 # puts name + lname
 
+puts "Enter your name:"
+name = gets.chomp
+
+puts "Enter something you own:"
+own = gets.chomp
+
+greeting = "Hello, #{name}. What an excellent #{own} you have!"
+puts greeting
+
