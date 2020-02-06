821ebc17c34532c6446cf2b60c0a3e1b73d4e88f
Luis Rosado <luismrosado@yahoo.com>
2020-02-06 07:05:58 +0000

    Math & Numbers

--- math.rb
+++ math.rb
@@ -1,2 +1,49 @@
-number = 10
-puts number
+# number = 10
+# puts number
+
+# val1 = 10
+# val2 = 10.0
+# val3 = "10"
+
+# puts "#{val1.inspect} is the type #{val1.class}"
+# puts "#{val2.inspect} is the type #{val2.class}"
+# puts "#{val3.inspect} is the type #{val3.class}"
+
+# val1 = 10 + 25
+# puts val1
+
+# val1 = 10 + 25 * 4
+# puts val1
+
+# val1 = (10 + 25) * 4
+# puts val1
+
+# val = 3.0 + 2.0 + 1.0 - 5.0 - 1.0 / 4.0 + 6.0
+# puts val
+
+# val1 = 2.5 * 4
+# puts val1
+
+# val1 = 2
+# val2 = val1.to_i
+# puts val2
+
+# val = 11 / 2
+# puts val
+
+# val = 11.to_f / 2 
+# puts val
+
+# val = "macaroni"
+# val2 = " & cheese"
+# puts val + val2
+
+# val1 = "Hello"
+# puts val1 * 10
+
+# val = 10 * 4 + 30 * 2
+# puts val
+
+puts "Enter length in inches:"
+val = gets.to_i * 2.54
+puts "that is #{val} centimeters"
