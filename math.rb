8e453f0e07bcfced3a0389976f839c82d62d807b
Luis Rosado <luismrosado@yahoo.com>
2020-02-09 15:42:18 +0000

    methods refresher

--- math.rb
+++ math.rb
@@ -71,12 +71,19 @@
 # val = double(10)
 # puts val
 
+def square(n)
+    n * n
+end
+
 def pythagorean_theorem(a, b)
-    a_squared = a * a
-    b_squared = b * b
+    a_squared = square(a)
+    b_squared = square(b)
     a_squared_plus_b_squared = a_squared + b_squared
 
     return Math.sqrt(a_squared_plus_b_squared)
 end
 
-puts pythagorean_theorem(3, 4)
+puts pythagorean_theorem(5, 12)
+
+
+
